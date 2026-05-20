7b69639f00faa8cb9aa85246d5396019cfd896e0
Muhammad Faiz <m.faizmuhammad2017@gmail.com>
2026-05-21 01:22:09 +0500

    upd: Render API path

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "https://micasa.backend.saqlainali.tech/rest/v1";
+const baseURL = "https://micasa-xdxi.onrender.com/rest/v1";
 const normalizedBaseURL = baseURL.replace(/\/+$/, '');
 
 const apiClient = axios.create({
