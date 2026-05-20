3191cefcbe6ce8ff66fe64578edfbbe54748179b
Muhammad Faiz <m.faizmuhammad2017@gmail.com>
2026-05-20 21:47:19 +0500

    upd: Updated data-source for ssl

--- backend/src/configs/data-source.js
+++ backend/src/configs/data-source.js
@@ -9,16 +9,19 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 5432),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: process.env.NODE_ENV === "development", // Enable auto-sync only in development
-    logging: false,
-    entities: [path.resolve(__dirname, "../entities/*.js")],
-    migrations: [path.resolve(__dirname, "../../migrations/*.js")],
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 18732),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.NODE_ENV === "development", // Enable auto-sync only in development
+  logging: false,
+  entities: [path.resolve(__dirname, "../entities/*.js")],
+  migrations: [path.resolve(__dirname, "../../migrations/*.js")],
+  ssl: {
+    ca: process.env.DB_CA_CERT,
+  },
 });
 
 export default AppDataSource;
